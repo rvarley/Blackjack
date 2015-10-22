601bcfe2f2b5549c5026ca1b9a4a439c2ae7952a
ransomv <rvarley2001@yahoo.com>
2015-10-22 09:39:39 -0700

    updates

--- cardTests.py
+++ cardTests.py
@@ -4,11 +4,27 @@ card1 = Card(1, 1)
 card2 = Card(3, 13)
 card3 = Card(4, 13)
 card4 = Card(3, 14)
+
 print("card1, 1, 1 is: ", card1)
 print("card2 3, 13 is: ", card2)
 assert card1.__str__() == "Ace of Diamonds"
 assert card2.__str__() == "King of Spades"
-# The following 2 asserts will be added as an enhancement - time permitting
-# assert card3.__str__() == "Suit out of range"  
-# assert card4.__str__() == "Rank out of range"
-print("Assertions Passed!!!")
+print("Pretty printing assertions Passed!!!")
+
+assert card1.cmpSort(card2) == -1
+assert card2.cmpSort(card1) == 1
+assert card2.cmpSort(card2) == 0
+print("cmpSort Assertions Passed!!!")
+
+assert card1.cmp(card2) == -1
+assert card2.cmp(card1) == 1
+assert card2.cmp(card2) == 0
+print("Cmp Assertions Passed!!!")
+
+assert bool(card1.rank > card2.rank) == False
+assert bool(card1.rank >= card2.rank) == False
+assert bool(card1.rank < card2.rank) == True
+assert bool(card1.rank <= card2.rank) == True
+assert bool(card1.rank == card2.rank) == False
+assert bool(card1.rank == card1.rank) == True
+print("Comparison overloading assertions passed")
